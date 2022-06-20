d956a589c430af7725aa191d67dae2244a98f49f
SpinnerX <heraaronhotmail@yahoo.com>
2022-06-20 01:24:49 +0000

    Main Src File

---
+++ LinearAlgebraFramework/main.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "include/Matrix.h"
+#include <type_traits>
+using namespace std;
+
+/*
+
+To acheive templates that only take type of integrals (meaning integers, floats, doubles, etc..)
+- Theres a technique called tag dispatching
+
+*/
+
+int main(){
+    
+}                                 
