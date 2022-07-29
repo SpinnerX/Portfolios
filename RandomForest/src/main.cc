f7353b0116e7edcd9ffe8954c1363246e58e5504
SpinnerX <heraaronhotmail@yahoo.com>
2022-07-29 10:23:35 -0700

    Src File

--- RandomForest/src/main.cc
+++ RandomForest/src/main.cc
@@ -2,8 +2,15 @@
 #include <cstring>
 #include <iomanip>
 #include <algorithm>
+#include "../include/Vector/Vec2.h"
 using namespace std;
 
 int main(){
+    Coords<double> coord1 = Coords<double>(2., 2.);
+    Vec2<double> vec = Vec2<double>(coord1);
+
+    cout << vec.length() << endl;
+    
+
     
 }
